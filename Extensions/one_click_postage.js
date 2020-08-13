892952727caefba4406b2433a2db7f4c9b55c357
marcustyphoon <marcustyphoon@gmail.com>
2020-08-13 06:39:49 -0700

    [One-Click Postage] fix on non-react pages (e.g. /liked/by) (#1960)

--- Extensions/one_click_postage.js
+++ Extensions/one_click_postage.js
@@ -1,5 +1,5 @@
 //* TITLE One-Click Postage **//
-//* VERSION 4.4.17 **//
+//* VERSION 4.4.18 **//
 //* DESCRIPTION Lets you easily reblog, draft and queue posts **//
 //* DEVELOPER new-xkit **//
 //* FRAME false **//
@@ -650,14 +650,17 @@ XKit.extensions.one_click_postage = new Object({
 			if ($(this).hasClass("selected") === true) { m_value = "true"; }
 			XKit.storage.set("one_click_postage", "share_on_" + $(this).attr('data-site'), m_value);
 		});
-		
-		const reblogAriaLabel = await XKit.interface.translate('Reblog');
-		const reblog_buttons = [
+
+		var reblog_buttons = [
 			'.reblog_button',
 			'.post_control.reblog',
 			'button[aria-label="Reblog"]',
-			`a[aria-label="${reblogAriaLabel}"][href*="/reblog/"]`
 		].join(',');
+
+		if (XKit.page.react) {
+			const reblogAriaLabel = await XKit.interface.translate('Reblog');
+			reblog_buttons += `, a[aria-label="${reblogAriaLabel}"][href*="/reblog/"]`;
+		}
 
 		$(document).on("mouseover", reblog_buttons, function(event) {
 			if ($(this).hasClass("radar_button") === true) {return; }
