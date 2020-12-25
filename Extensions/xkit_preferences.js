e8e4db915b0347f2b2b56923163ec8f9be019dd2
motackt <42635772+motackt@users.noreply.github.com>
2020-12-26 04:08:17 +1100

    [Activity+] Remove Extension (#2025)

--- Extensions/xkit_preferences.js
+++ Extensions/xkit_preferences.js
@@ -1,5 +1,5 @@
 //* TITLE XKit Preferences **//
-//* VERSION 7.6.11 **//
+//* VERSION 7.6.12 **//
 //* DESCRIPTION Lets you customize XKit **//
 //* DEVELOPER new-xkit **//
 
@@ -173,7 +173,7 @@ XKit.extensions.xkit_preferences = new Object({
 
 	spring_cleaning: function() {
 
-		var clean_list = ["unreverse", "filter_by_type", "XIM", "yahoo", "reblog_as_text", "reblog_yourself", "alternative_timestamps", "autoloadimages", "soft_refresh", "lethe"];
+		var clean_list = ["unreverse", "filter_by_type", "XIM", "yahoo", "reblog_as_text", "reblog_yourself", "alternative_timestamps", "autoloadimages", "soft_refresh", "lethe", "activity_plus"];
 
 		var removed_list = [];
 
